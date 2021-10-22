3a1a681e3d9ea8e4c73112cafafe9cee6aee0fc0
Einav1 <einavshpigel@gmail.com>
2021-10-22 11:33:39 +0300

    basic files

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+import Layout from "../components/Layout";
+export default function about() {
+  return (
+    <Layout title="About Dj Events">
+      <h1>About</h1>
+      <p>This is an app to find the latest DJ and other musical events</p>
+    </Layout>
+  );
+}
